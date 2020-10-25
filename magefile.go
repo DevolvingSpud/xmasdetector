4b455d5b6f300662c98d7dfd164b5a092c9ea1a5
DevolvingSpud <nate.carpenter@gmail.com>
2020-10-25 14:55:19 -0400

    Added magefile

---
+++ magefile.go
@@ -0,0 +1,26 @@
+// +build mage
+
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	// mg contains helpful utility functions, like Deps
+)
+
+// Default target to run when none is specified
+// If not set, running mage will list available targets
+// var Default = Build
+
+// A build step that requires additional params, or platform specific steps for example
+func Build() error {
+	fmt.Println("Tidying...")
+	cmd := exec.Command("go", "mod", "tidy")
+
+	fmt.Println("Formatting...")
+	cmd = exec.Command("go", "fmt", "-x")
+
+	fmt.Println("Installing...")
+	cmd = exec.Command("go", "install")
+	return cmd.Run()
+}
